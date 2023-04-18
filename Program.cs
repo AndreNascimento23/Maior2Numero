9996288dbba0318ecafcad4c97957142cdf352b7
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-18 14:44:13 -0300

    Executavel

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+double numero1, numero2;
+
+Console.WriteLine(" --- Qual Número é Maior ---\n");
+
+Console.Write("Digite o primeiro número: ");
+numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+numero2 = Convert.ToDouble(Console.ReadLine());
+
+if (numero1 > numero2)
+{
+    Console.WriteLine($"O maior número é {numero1}");
+}
+else if (numero1 < numero2)
+{
+    Console.WriteLine($"O maior número é {numero2}");
+}
+else
+{
+    Console.WriteLine(" --- Os dois números são iguais ---");
+}
